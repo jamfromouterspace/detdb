f25e8789de807137dca244f6d69caeff2139f893
Jamiel Rahi <jamiel.nr@gmail.com>
2019-05-14 21:36:32 -0400

    Create landing page

--- detdb/detdb/settings.py
+++ detdb/detdb/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'landing',
+    'browse_db'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
--- detdb/detdb/urls.py
+++ detdb/detdb/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('landing.urls')),
     path('db/', include('browse_db.urls')),
     path('admin/', admin.site.urls),
 ]
