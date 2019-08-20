c1ba3dda6e85f664616affcbb356d8fe470f2c47
Jamiel Rahi <jamiel.nr@gmail.com>
2019-08-20 07:07:57 -0400

    Create Searches table to keep track of app search history

--- db-files/init.sql
+++ db-files/init.sql
@@ -233,3 +233,12 @@ CREATE TABLE common_fuels (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	chemical VARCHAR(10) UNIQUE NOT NULL
 );
+
+
+-- Keep a record of searches for extreme data mining
+CREATE TABLE searches (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	query VARCHAR(100) NOT NULL,
+	ipv4_address VARCHAR(15),
+	created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
