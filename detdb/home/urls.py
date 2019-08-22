0cd8a48b6c01057cec67047c9f00f57cecdc4893
Jamiel Rahi <jamiel.nr@gmail.com>
2019-08-22 08:45:17 -0400

    Add icons and make small CSS adjustments

--- detdb/home/urls.py
+++ detdb/home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.coming_soon, name='coming_soon'),
+    path('download/', views.coming_soon, name='coming_soon'),
+    path('cite/', views.coming_soon, name='coming_soon'),
+    path('help/', views.coming_soon, name='coming_soon'),
 ]
